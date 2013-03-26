56734bf0b32c89528bbe37e029b6c2a3bd444327
A Riley <me@ariley.me>
2013-03-26 08:46:56 +0000

    First commit
    Created project

---
+++ js/script2.js
@@ -0,0 +1,39 @@
+// input field
+$("#go").click(function(){
+	showResults($("#search").val());
+})
+
+function showResults(str) {
+	if (str.length==0) { 
+		$("#results").empty();
+		return;
+	}
+	$.ajax({
+		dataType: "jsonp",
+		url: "http://search.twitter.com/search.json?q="+encodeURIComponent(str),
+		success: function(result) {
+			$("#results").empty();
+			for (var i = 0; i < result.results.length; i++){
+				var thisTweet = replaceWithData($("#tweet-template").html(), result.results[i]);
+				$("#results").append(thisTweet);
+			}
+		},
+		fail: function() {
+			return false;
+		},
+	});
+}
+
+// Replace the {{XXX}} with the corresponding property
+function replaceWithData(template, data) {
+	var html_template = template, 
+		prop;
+	for (prop in data) {
+		html_template = html_template.replace('{{' + prop + '}}', data[prop]);
+	}
+	return html_template;
+}
+
+
+
+
